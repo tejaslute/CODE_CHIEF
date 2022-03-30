e0f3447062ca346cb34a19f18a3ab2bf45014bc7
Tejas <tejaslute24@gmail.com>
2022-03-30 16:48:30 +0530

    example

---
+++ Economics_Class.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while (t--) {
+		int m;
+		cin >> m;
+		int s[m], d[m];
+		int count = 0;
+		for (int i = 0; i < m; i++) {
+			cin >> s[i];
+		}
+		for (int i = 0; i < m; i++) {
+			cin >> d[i];
+		}
+		for (int i = 0; i < m; i++) {
+
+			if (s[i] == d[i]) {
+				count++;
+			}
+		}
+		cout << count << endl;
+	}
+	return 0;
+}
---
+++ INCRIQ.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b;
+	b = 170;
+	cin >> a;
+	if (a + 7 > b) cout << "YES" << endl;
+	else cout << "NO" << endl;
+	return 0;
+}
