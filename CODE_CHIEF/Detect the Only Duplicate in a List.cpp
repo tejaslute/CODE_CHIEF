0a3a16a3c3c553bdaacd3402777525227383ee95
Tejas Lute <91754694+tejaslute@users.noreply.github.com>
2022-08-25 10:35:37 +0530

    Create Detect the Only Duplicate in a List.cpp

---
+++ CODE_CHIEF/Detect the Only Duplicate in a List.cpp
@@ -0,0 +1,17 @@
+int solve(vector<int>& nums) {
+    
+    unordered_map<int,int>mp;
+    for(int i=0;i<nums.size();i++)
+
+    {
+        mp[nums[i]]++;
+    }
+    for(auto it:mp)
+    {
+        if(it.second==2)
+        {
+            return it.first;
+        }
+    }
+    return 0;
+}
